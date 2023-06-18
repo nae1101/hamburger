fe94a2e60a06567416c081caa295317c645267bf
hienae.design@gmail.com <hienae.design@gmail.com>
2023-06-19 00:36:40 +0900

    jQuery追加

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// SPとTabのとき
+// Menuをクリックしたら画面が暗くなり右からサイドバーがスライドしてくる。
+// バツ印を押したらサイドバーが右にスライドして画面が明るくなる。
+
+jQuery(function($){
+    //Menuをクリックしたら
+    $(".js-sidebar-menu").on("click",function(){
+        alert('テスト');
+
+        //画面が暗くなる
+        $(".l-body__overlay").fadeIn(500).toggleClass("is-open");
+        //右からサイドバーがスライドしてくる
+        $(".l-sidebar").toggleClass("is-open");
+    })
+    
+})
