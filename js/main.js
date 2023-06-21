b7bb73906ac3d959e4207ef69de4d38febbbe8ca
hienae.design@gmail.com <hienae.design@gmail.com>
2023-06-21 10:54:55 +0900

    第一回提出

--- js/main.js
+++ js/main.js
@@ -6,12 +6,29 @@ jQuery(function($){
 
     //Menuをクリックしたら
     $(".js-sidebar-menu").on("click",function(){
-        alert('テスト');
-
         //画面が暗くなる
-        $(".l-body__overlay").fadeIn(500).toggleClass("is-open");
+        $(".l-body__overlay").toggleClass("is-open");
+        //bodyが固定される
+        $("#body").css("position","fixed");
         //右からサイドバーがスライドしてくる
         $(".l-sidebar").toggleClass("is-open");
+    })
+    //バツをクリックしたら
+    $(".js-icon__close").on("click",function(){
+        //サイドバーが戻り
+        $(".l-body__overlay").removeClass("is-open");
+        //bodyの固定がなくなる
+        $("#body").css("position","");
+        //画面が明るくなる
+        $(".l-sidebar").removeClass("is-open");
+
+    })
+    // 画面幅が変更されたときに実行させたい処理内容
+    $(window).resize(function(){
+        $(".l-body__overlay").removeClass("is-open");
+        $("#body").css("position","");
+        $(".l-sidebar").removeClass("is-open");
+
     })
     
 })
